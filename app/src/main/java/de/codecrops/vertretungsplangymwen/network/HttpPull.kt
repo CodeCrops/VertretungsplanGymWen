2a6a2352dff0f82d3b222a04df1cd0abe62d1716
K1TR1K <lucalli2000@googlemail.com>
2018-09-24 23:11:08 +0200

    Wiederherstellung des codes

---
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpPull.kt
@@ -0,0 +1,12 @@
+package de.codecrops.vertretungsplangymwen.network
+
+object HttpPull {
+    fun getToday(): String {
+        val getRequest = HttpGetRequest()
+        return getRequest.execute("http://gym-wen.de/vp/heute.htm").get()
+    }
+    fun getTomorrow(): String {
+        val getRequest = HttpGetRequest()
+        return getRequest.execute("http://gym-wen.de/vp/morgen.htm").get()
+    }
+}
