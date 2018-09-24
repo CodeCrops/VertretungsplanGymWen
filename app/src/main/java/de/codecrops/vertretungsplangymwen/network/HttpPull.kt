7d3fb9ec8bab8120385942d30622daead8abf621
BuildTools <unconfigured@null.spigotmc.org>
2018-09-24 20:21:48 +0200

    Htm Datei Download Implementiert, funktioniert aber nicht.

---
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpPull.kt
@@ -0,0 +1,13 @@
+package de.codecrops.vertretungsplangymwen.network
+
+object HttpPull {
+    fun getToday(): String {
+        val getRequest = HttpGetRequest()
+        return getRequest.execute("http://gym-wen.de/vp/heute.htm").get()
+    }
+
+    fun getTomorrow(): String {
+        val getRequest = HttpGetRequest()
+        return getRequest.execute("http://gym-wen.de/vp/morgen.htm").get()
+    }
+}
