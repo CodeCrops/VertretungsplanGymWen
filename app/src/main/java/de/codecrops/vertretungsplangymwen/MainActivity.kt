b8d21a42f2843818eadd304a1dc106ddcc44efc5
BuildTools <unconfigured@null.spigotmc.org>
2018-09-24 21:01:21 +0200

    Test Code von Master auf implementation Branch gemoved.

--- app/src/main/java/de/codecrops/vertretungsplangymwen/MainActivity.kt
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/MainActivity.kt
@@ -2,15 +2,11 @@ package de.codecrops.vertretungsplangymwen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import de.codecrops.vertretungsplangymwen.network.HttpPull
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        text.setText(HttpPull.getToday())
     }
 }
