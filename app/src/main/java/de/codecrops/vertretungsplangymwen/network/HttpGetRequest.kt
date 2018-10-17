9d57a603e919f8b3c6b5a8a1c9aade07c6461141
Luca Preibsch <lucalli2000@googlemail.com>
2018-10-17 13:48:03 +0200

    Der Download der html Datei funktioniert jetzt einwandfrei. Entsprechende Klassen wurden hinzugefügt. TODO: Nutzername und Passwort müssen durch Login gesetzt werden.

--- app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpGetRequest.kt
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpGetRequest.kt
@@ -3,45 +3,97 @@ package de.codecrops.vertretungsplangymwen.network
 import android.os.AsyncTask
 import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.lang.StringBuilder
 import java.net.HttpURLConnection
+import java.net.MalformedURLException
 import java.net.URL
+import java.nio.charset.Charset
+import android.util.Base64
+
+/**
+ * Diese Klasse bietet als AsyncTask eine asynchrone Methode um die html Datei der mitgegebenen URL
+ * als String zurückzugeben.
+ */
 
 class HttpGetRequest : AsyncTask<String, Void, String>() {
-    private val requestMethod = "GET"
-    private val readTimeout = 15000
-    private val connectionTimeout = 15000
 
+    /**
+     * Diese Methode initialisiert den Download der Datei und gibt den fertigen String zurück
+     * @param params Die Ziel-URL
+     * @return der String der html Datei
+     */
     override fun doInBackground(vararg params: String?): String {
-        val stringUrl = params[0]
+        val url = createUrl(params[0]!!)
+        var response = ""
+        try {
+            response = makeHttpRequest(url!!)
+        } catch (e: IOException) {
+        }
+        return response
+    }
+
+    //Erstellt eine URL aus dem URL-Adressen-String
+    private fun createUrl(stringUrl: String): URL? {
+        try {
+            return URL(stringUrl)
+        } catch (e: MalformedURLException) {
+        }
+        return null
+    }
+
+    /**
+     * Erstellt die eigentliche URL Verbindung, liest die html Datei aus und schließt die
+     * Verbindung wieder.
+     * @param url Die URL der zu herunterladenden html Datei
+     * @return Der String der html Datei
+     */
+    private fun makeHttpRequest(url: URL): String  {
+        var response = ""
+        lateinit var  urlConnection: HttpURLConnection
+        lateinit var inputStream: InputStream
+
+        //Username:Password TODO: automatisches Setzen der Werte durch Anmeldung
+        val userpasswort = ""
+
+        //erstellt einen Authentication key aus Nutzername und Passwort mithilfe von Base64
+        val encAutorization = Base64.encodeToString(userpasswort.toByteArray(), 0)
+                .replace("\n", "")
 
         try {
-            val Url = URL(stringUrl)
-            val connection = Url.openConnection() as HttpURLConnection
+            urlConnection = url.openConnection() as HttpURLConnection
 
-            connection.requestMethod = requestMethod
-            connection.readTimeout = readTimeout
-            connection.connectTimeout = connectionTimeout
+            //Setzt den Authentication Key für die Verbindung
+            urlConnection.setRequestProperty("Authorization", "Basic $encAutorization")
+            urlConnection.requestMethod = "GET"
+            urlConnection.readTimeout = 20000
+            urlConnection.connectTimeout = 20000
+            urlConnection.connect()
 
-            connection.connect()
-
-            val streamReader = InputStreamReader(connection.inputStream)
-            val reader = BufferedReader(streamReader)
-            val sb = StringBuilder()
-
-            if (reader.readLine() != null) {
-                sb.append(reader.readLine())
+            if(urlConnection.responseCode == 200) {
+                inputStream = urlConnection.inputStream
+                response = readFromStream(inputStream)
             }
-
-            reader.close()
-            streamReader.close()
-
-            return sb.toString()
         } catch (e: IOException) {
-            e.printStackTrace();
-        }
 
-        return "Fail"
+        } finally {
+            urlConnection.disconnect()
+            inputStream.close()
+        }
+        return response
+    }
+
+    private fun readFromStream(inputStream: InputStream): String {
+        val output = StringBuilder()
+        val inputStreamReader = InputStreamReader(inputStream, Charset.forName("UTF-8"))
+        val reader = BufferedReader(inputStreamReader)
+        var line = reader.readLine()
+        while (line != null) {
+            output.append(line)
+            line = reader.readLine()
+        }
+        return output.toString()
     }
 
     override fun onPostExecute(result: String) {
--- app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpPull.kt
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/network/HttpPull.kt
@@ -1,10 +1,18 @@
 package de.codecrops.vertretungsplangymwen.network
 
+/**
+ * Diese Klasse bietet jeweils eine Methode, um die html Datei des heutigen oder morigen Tages als
+ * String zu bekommen.
+ */
 object HttpPull {
+
+    //Gibt die Datei des aktuellen Tages zurück
     fun getToday(): String {
         val getRequest = HttpGetRequest()
         return getRequest.execute("http://gym-wen.de/vp/heute.htm").get()
     }
+
+    //Gibt die Datei des morigen Tages zurück
     fun getTomorrow(): String {
         val getRequest = HttpGetRequest()
         return getRequest.execute("http://gym-wen.de/vp/morgen.htm").get()
