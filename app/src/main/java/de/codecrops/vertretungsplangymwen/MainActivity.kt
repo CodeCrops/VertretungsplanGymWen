bae7d4407ed5459864d583325548f3e5347d6417
K1TR1K <31764354+K1TR1K@users.noreply.github.com>
2018-10-25 11:41:27 +0200

    Kommentar entfernt

--- app/src/main/java/de/codecrops/vertretungsplangymwen/MainActivity.kt
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
-
-        //text.setText(HttpPull.getToday())
     }
 
     private fun createNotificationChannel() {
