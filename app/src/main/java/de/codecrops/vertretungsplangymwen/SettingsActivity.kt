480ce841cf135048c9c7e58c6d84118dac60ee47
Yannik <atomtoasty@gmail.com>
2019-01-19 12:58:30 +0100

    R-Save (nur dafür da, damit ich das projekt neu laden kann)

---
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package de.codecrops.vertretungsplangymwen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.xml.settings_refresh)
+    }
+}
---
+++ app/src/main/java/de/codecrops/vertretungsplangymwen/gui/customFragments/SettingsRefreshFragment.kt
@@ -0,0 +1,20 @@
+package de.codecrops.vertretungsplangymwen.gui.customFragments
+
+
+import android.os.Bundle
+import android.support.v14.preference.PreferenceFragment
+import android.support.v7.preference.PreferenceFragmentCompat
+import de.codecrops.vertretungsplangymwen.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class SettingsRefreshFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_refresh, rootKey)
+    }
+
+}
